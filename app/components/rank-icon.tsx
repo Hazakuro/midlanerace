c9b1a20053b3a3e82bbaf5b114985601290d26cf
Hazakuro <hazakuro1@gmail.com>
2026-09-16 21:10:03 +0300

    Use uploaded WebP rank icons locally

--- app/components/rank-icon.tsx
+++ app/components/rank-icon.tsx
@@ -1,18 +1,16 @@
 import {getRankTier} from '@/lib/rank';
 
-const iconBase = 'https://static.wikia.nocookie.net/leagueoflegends/images/';
-
 const iconFiles: Record<string, string> = {
   Iron: 'https://leagueoflegends.fandom.com/wiki/Special:FilePath/Season%202023%20-%20Iron.png',
   Gold: 'https://leagueoflegends.fandom.com/wiki/Special:FilePath/Season%202023%20-%20Gold.png',
-  Bronze: `${iconBase}c/cb/Season_2023_-_Bronze.png/revision/latest/scale-to-width-down/130?cb=20231007195824`,
-  Silver: `${iconBase}c/c4/Season_2023_-_Silver.png/revision/latest/scale-to-width-down/130?cb=20231007195834`,
-  Platinum: `${iconBase}b/bd/Season_2023_-_Platinum.png/revision/latest/scale-to-width-down/130?cb=20231007195833`,
-  Emerald: `${iconBase}4/4b/Season_2023_-_Emerald.png/revision/latest/scale-to-width-down/130?cb=20231007195827`,
-  Diamond: `${iconBase}3/37/Season_2023_-_Diamond.png/revision/latest/scale-to-width-down/130?cb=20231007195826`,
-  Master: `${iconBase}d/d5/Season_2023_-_Master.png/revision/latest/scale-to-width-down/130?cb=20231007195832`,
-  Grandmaster: `${iconBase}6/64/Season_2023_-_Grandmaster.png/revision/latest/scale-to-width-down/130?cb=20231007195830`,
-  Challenger: `${iconBase}1/14/Season_2023_-_Challenger.png/revision/latest/scale-to-width-down/130?cb=20231007195825`,
+  Bronze: '/ranks/Season_2023_-_Bronze.webp',
+  Silver: '/ranks/Season_2023_-_Silver.webp',
+  Platinum: '/ranks/Season_2023_-_Platinum.webp',
+  Emerald: '/ranks/Season_2023_-_Emerald.webp',
+  Diamond: '/ranks/Season_2023_-_Diamond.webp',
+  Master: '/ranks/Season_2023_-_Master.webp',
+  Grandmaster: '/ranks/Season_2023_-_Grandmaster.webp',
+  Challenger: '/ranks/Season_2023_-_Challenger.webp',
 };
 
 export function RankIcon({ rank, size = 38 }: { rank: string | null | undefined; size?: number }) {
